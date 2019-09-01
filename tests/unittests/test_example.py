f47625bf189f7a6f8cbb4bf2c9d7e37934c8b0e0
Prateek Gurjar <prateek.gurjar@vecna.com>
2019-09-01 14:07:13 -0400

    update uniitest script

--- tests/unittests/test_example.py
+++ tests/unittests/test_example.py
@@ -2,18 +2,17 @@ import unittest
 from datetime import datetime
 from airflow import DAG
 from airflow.models import TaskInstance
-from airflow.operators import BashOperator
+from airflow.operators.python_operator import PythonOperator
 
+def ret_hello():
+    hello = 'Hello'
+    return hello
 
 class TestExample(unittest.TestCase):
 
     def test_execute(self):
         dag = DAG(dag_id='example', start_date=datetime.now())
-        task = BashOperator(dag=dag, task_id='print_date')
+        task = PythonOperator(python_callable=ret_hello, task_id='test_python', dag=dag)
         ti = TaskInstance(task=task, execution_date=datetime.now())
         result = task.execute(ti.get_template_context())
-        self.assertEqual(result, '2019-08-31')
-
-
-suite = unittest.TestLoader().loadTestsFromTestCase(TestExample)
-unittest.TextTestRunner(verbosity=2).run(suite)
+        self.assertEqual(result, 'Hello')
