6663cbca360bd58c7f1c7e812671e4ea0c339a68
Prateek Gurjar <prateek.gurjar@vecna.com>
2019-09-02 01:52:57 -0400

    folder reorg

--- airflow/dags/lib/helpers.py
+++ airflow/dags/lib/helpers.py
@@ -1,40 +1,48 @@
 import requests, pandas, boto3, os, configparser, datetime, logging
 from io import BytesIO, StringIO
 from zipfile import ZipFile
+from airflow.contrib.hooks.aws_hook import AwsHook
 
-def get_aws_config(profile, key):
+def get_aws_config(conn_id):
+
+	aws_hook = AwsHook(conn_id)
+	credentials = aws_hook.get_credentials()
+	return credentials
+
+def get_aws_config_deprecated(profile, key):
+
 	config = configparser.ConfigParser()
 	config.read(f'{os.environ["AWS_CREDS"]}/credentials')
 	return config[profile][key]
 
 def download_extract(url):
 
-	logging.info(f'Downloading dataset from {url}')
-	logging.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+	print(f'Downloading dataset from {url}')
+	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 	response = requests.get(url, stream=True)
-	logging.info('Download Complete')
-	logging.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+	print('Download Complete')
+	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
-	logging.info('Unzipping response to byte stream')
-	logging.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+	print('Unzipping response to byte stream')
+	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 	z = ZipFile(BytesIO(response.content))
-	logging.info('Unzipping response Complete')
-	logging.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+	print('Unzipping response Complete')
+	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 	return z
 
 def dataframe_to_s3(s3_client, input_datafame, bucket_name, file_info):
 
 	csv_buffer = StringIO()
-	input_datafame.to_csv(csv_buffer)
+	input_datafame.to_csv(csv_buffer, index=False)
 
 	filename = file_info[0]
 	filepath = file_info[1]
 
 	s3_client.put_object(Bucket=bucket_name, Key=filepath, Body=csv_buffer.getvalue())
-	logging.info(f'{filename} successfully loaded to s3')
+	print(f'{filename} successfully loaded to s3')
 	
 def get_file_info(start_date):
 
@@ -48,34 +56,56 @@ def get_file_info(start_date):
 	file_info = (filename, filepath)
 	return file_info
 
-def load_historical_trips(url, s3_client, bucket_name):
+def set_logging():
+	logging.getLogger().setLevel(logging.INFO)
+	logging.basicConfig(format='%(asctime)s %(levelname)-s %(message)s',level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+def load_historical(**kwargs):
+
+	url = kwargs['url']
+	bucket_name = kwargs['bucket_name']
+	load = kwargs['load']
+
+	s3_client = boto3.client('s3', aws_access_key_id=get_aws_config('aws_credentials')[0], aws_secret_access_key=get_aws_config('aws_credentials')[1])
 
 	z = download_extract(url)
 
 	text_files = z.infolist()
 
-	for text_file in text_files:
+	if load=='trips':
 
-		if 'od' in text_file.filename.lower():
-			logging.info(f'starting load for {text_file.filename}')
-			start_date = datetime.datetime.strptime(text_file.filename.split('_')[1].split('.')[0], "%Y-%m")
-			df = pandas.read_csv(z.open(text_file.filename))
-			year = start_date.strftime("%Y")
-			month = start_date.strftime("%m")
-			filepath = f'trips/{year}/{month}/{text_file.filename}'
-			file_info = (text_file.filename, filepath)
-			dataframe_to_s3(s3_client, df, bucket_name, file_info)
+		for text_file in text_files:
 
-def set_logging():
-	logging.getLogger().setLevel(logging.INFO)
-	logging.basicConfig(format='%(asctime)s %(levelname)-s %(message)s',level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+			if 'od' in text_file.filename.lower():
+				print(f'starting load for {text_file.filename}')
+				start_date = datetime.datetime.strptime(text_file.filename.split('_')[1].split('.')[0], "%Y-%m")
+				df = pandas.read_csv(z.open(text_file.filename))
+				year = start_date.strftime("%Y")
+				month = start_date.strftime("%m")
+				filepath = f'trips/{year}/{month}/{text_file.filename}'
+				file_info = (text_file.filename, filepath)
+				dataframe_to_s3(s3_client, df, bucket_name, file_info)
+
+	elif load=='stations':
+
+		for text_file in text_files:
+
+			if 'station' in text_file.filename.lower():
+				print(f'starting load for {text_file.filename}')
+				start_date = datetime.datetime.strptime(text_file.filename.split('_')[1].split('.')[0], "%Y")
+				df = pandas.read_csv(z.open(text_file.filename))
+				year = start_date.strftime("%Y")
+				filepath = f'station/{year}/{text_file.filename}'
+				file_info = (text_file.filename, filepath)
+				dataframe_to_s3(s3_client, df, bucket_name, file_info)
+
 
 def dataframe_to_s3_loader():
 
 	for text_file in text_files:
 		#logging.info(text_file.filename)
 		if text_file.filename == 'OD_2019-07.csv':
-			logging.info(f'starting load for {text_file.filename}')
+			print(f'starting load for {text_file.filename}')
 			df = pandas.read_csv(z.open(text_file.filename))
 
 			# start_date = datetime.datetime.strptime(df.get_value(0,'start_date'), "%Y-%m-%d %H:%M:%S")
@@ -112,11 +142,3 @@ def dataframe_to_s3_loader():
 				file_info = get_file_info(start_date)
 				dataframe_to_s3(s3_client, df_interval, 'bixi.qc.raw', file_info)
 				start_date = end_date
-
-if __name__ == '__main__':
-
-	set_logging()
-	url=f'https://montreal.bixi.com/c/bixi/file_db/data_all.file/BixiMontrealRentals2019.zip'
-	bucket_name = 'bixi.qc.raw'
-	s3_client = boto3.client('s3', aws_access_key_id=get_aws_config('bixi','aws_access_key_id'), aws_secret_access_key=get_aws_config('bixi', 'aws_secret_access_key'))
-	load_historical_trips(url, s3_client, bucket_name)
